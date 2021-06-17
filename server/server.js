73d11c16d5390f6dba69af3d783099a5bcd6c601
Nathan Albers <nathan@basecamp-health.com>
2021-06-17 02:07:09 -0400

    connect to database and create Route template

--- server/server.js
+++ server/server.js
@@ -4,33 +4,155 @@ const cors = require("cors");
 
 const app = express();
 
+var Connection = require('tedious').Connection; 
+
+var config = {  
+    server: 'testdb-1.ccopgex7ytvv.us-east-1.rds.amazonaws.com', 
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'amazonian', 
+            password: 'zYca!GRJzw#H9w' 
+        }
+    },
+    options: {
+        encrypt: false,
+        database: 'acmeDB' 
+    }
+};  
+
+
+
+var connection = new Connection(config);  
+  connection.on('connect', function(err) {    
+    console.log("Connected");  
+  });
+
+connection.connect();
+
+var Request = require('tedious').Request;  
+var TYPES = require('tedious').TYPES;  
+
 var corsOptions = {
   origin: "http://localhost:3000"
 };
+
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// ALL ROUTES GO HERE:
 /** 
- *  example route:
- *    app.get("/endpoint-name", (req, res) => {
- *    
- *     // DB call would go here -> return a data set
- *     res.status(200).send(dataset);
- * })
- * 
- * */ 
-// simple route
-app.get("/home", (req, res) => {
-  res.json({ message: "Welcome to home." });
-});
+ROUTE TEMPLATE:
+
+// You will need to replace 'endpoint-name' with the name of your page (App.js)
+app.get("/endpoint-name", (req, res) => {
+    
+  // Put your SQL query here
+  request = new Request("SELECT * FROM TableName;", function(err) {  
+    if (err) {  
+        console.log(err);
+    }  
+  });
+
+  // The following code will store the values from the SQL query in an array of json objects called rows
+  let rows = [];
+  request.on('row', function(columns) { 
+    let jsonData = {};
+    columns.forEach(function(column) {  
+      if (column.value === null) {  
+        console.log('NULL');  
+      } else {  
+        jsonData[column.metadata.colName] = column.value;
+      }  
+    });  
+    rows = rows.concat(jsonData);
+  });  
+
+  // This will send the data from the SQL query to the front-end
+  request.on("requestCompleted", function (rowCount, more) {
+    res.json({ data: rows });
+  });
+
+  connection.execSql(request);
+ 
+ })
+
+*/ 
+
+
+// NATHAN'S ROUTES BEGIN
 
 app.get("/products", async (req, res) => {
-  res.status(200).send({ products: ["Product 1", "Product 2"]});
+
+  request = new Request("SELECT * FROM Product;", function(err) {  
+    if (err) {  
+        console.log(err);
+    }  
+  });
+
+  let rows = [];
+  request.on('row', function(columns) { 
+    let jsonData = {};
+    columns.forEach(function(column) {  
+      if (column.value === null) {  
+        console.log('NULL');  
+      } else {  
+        jsonData[column.metadata.colName] = column.value;
+      }  
+    });  
+    rows = rows.concat(jsonData);
+  });  
+
+  request.on("requestCompleted", function (rowCount, more) {
+    res.json({ products: rows });
+  });
+
+  connection.execSql(request);
+
 });
+
+app.get("/landing", (req, res) => {
+    
+  request = new Request("SELECT * FROM Employee;", function(err) {  
+    if (err) {  
+        console.log(err);
+    }  
+  });
+
+  let rows = [];
+  request.on('row', function(columns) { 
+    let jsonData = {};
+    columns.forEach(function(column) {  
+      if (column.value === null) {  
+        console.log('NULL');  
+      } else {  
+        jsonData[column.metadata.colName] = column.value;
+      }  
+    });  
+    rows = rows.concat(jsonData);
+  });  
+
+  request.on("requestCompleted", function (rowCount, more) {
+    res.json({ products: rows });
+  });
+
+  connection.execSql(request);
+ 
+ })
+
+// END OF NATHAN'S ROUTES
+
+// BEGINNING OF JOSH'S ROUTES
+
+
+// END OF JOSH'S ROUTES
+
+// BEGINNING OF HESTON'S ROUTES
+
+
+// END OF HESTON'S ROUTES
 
 
 // set port, listen for requests
